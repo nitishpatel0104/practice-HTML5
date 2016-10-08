5b57eeb663e476e102785ac101eaaeb7a9b1f811
msalmanzafar <m.salman.zafar@gmail.com>
2016-10-08 10:27:44 +0500

    update project page

--- Public Website/public/Project/Saylani Certificate/java.js
+++ Public Website/public/Project/Saylani Certificate/java.js
@@ -40,7 +40,8 @@ else{
 
 var i1 = "Enter Your Percentage for CSS-1: "
 var j1 = "CSS-1";
-var css = prompt(i1,j1);
+var e = prompt(i1,j1);
+var css =parseInt(e);
 
 if (css >= 90 && css < 100){
     var result2= css + "% A+";
@@ -60,7 +61,8 @@ else{
 
 var i2 = "Enter Your Percentage for CSS-2: "
 var j2 = "CSS-2";
-var css2 = prompt(i2,j2);
+var f = prompt(i2,j2);
+var css2 =parseInt(f);
 
 if (css2 >= 90 && css2 < 100){
     var result3= css2 + "% A+";
@@ -78,6 +80,24 @@ else{
     var result3= " Fail ";
 }
 
+var total1 = (((HTML + css + css2)/300)*100).toFixed(2);
+
+if (total1 >= 90 && total1 < 100){
+    var result4= total1 + "% A+";
+}
+else if(total1 >= 80 && total1 < 90){
+    var result4= total1 + "% A";
+}
+else if(total1 >= 70 && total1 < 80){
+    var result4= total1 + "% B";
+}
+else if(total1 >= 60 && total1 < 70){
+    var result4= total1 + "% C";
+}
+else{
+    var result4= " Fail ";
+}
+
 document.getElementById("name").innerHTML= a ;
 document.getElementById("no").innerHTML= b ;
 document.getElementById("father-name").innerHTML= c ;
@@ -85,4 +105,5 @@ document.getElementById("html-test").innerHTML=  result ;
 document.getElementById("css-test").innerHTML=  result2 ;
 document.getElementById("css2-test").innerHTML=  result3 ;
 
-document.getElementById("total").innerHTML=  total1 ;
+
+document.getElementById("total").innerHTML=  result4;
