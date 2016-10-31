977b9f9487615884bfc6f7e439e051c729bc1e4c
msalmanzafar <m.salman.zafar@gmail.com>
2016-11-01 01:41:53 +0500

    resturen

---
+++ Mass training lab task/29-10-2016/app.js
@@ -0,0 +1,75 @@
+/*
+var a ="2";
+var b ="2";
+var c="10";
+
+var function2 = (parseInt(a) + parseInt(b))* parseInt(c);
+
+function plus(a,b,c){
+    alert(function2);
+}
+*/
+
+// function plus(){
+//     var a =prompt("Please Enter you number for Plus:","1st");
+//     var b =prompt("Please Enter you number for Plus:","2st");
+//     var c=prompt("Please Enter you number for Multiply:","3st");
+//     var function2 = (parseInt(a) + parseInt(b))* parseInt(c);
+//     alert(function2);
+// }
+
+
+// function plus(){
+//     var a =prompt("Please Enter you number for Plus:","1st");
+//     var b =prompt("Please Enter you number for Plus:","2st");
+//     var c=prompt("Please Enter you number for Multiply:","3st");
+//     var function2 = (parseInt(a) + parseInt(b))* parseInt(c);
+//     if(isNaN(function2)){
+//         alert("Please Write Valid Number");
+//     }
+//     else
+//         alert(function2);
+// }
+
+// function squareNumber(aNumber){
+//     var result = aNumber * aNumber;
+//     return result;
+// }
+// var a =squareNumber(2);
+// var b =squareNumber(3);
+// var c =squareNumber(4);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// function arrayFunction(){
+//     var arr1 = prompt("Please Enter your Value:",);
+//     var arr2 = prompt("Please Enter your Value:",);
+//     var arr3 = prompt("Please Enter your Value:",);
+//     var totalvalues=[];
+//     for(var i = 0; i<totalvalues.length;i++){
+//         totalvalues =arr1.push();
+//         totalvalues =arr2.push();
+//         totalvalues =arr3.push();
+        
+//     }
+//     alert(totalvalues);
+    
+// }
+
+// function teaMake(){
+//     var tea ="";
+//     alert("Congratulation You have Tea");
+    
+// }
+// function milkTea(){
+//     alert("Congratulation You have Milk Tea");
+// }
+// function paratha(){
+//     alert("Congratulation You have Paratha");
+// }
+// function parathaWithTea(){
+//     alert("Congratulation You have Paratha With Tea");
+// }
+
