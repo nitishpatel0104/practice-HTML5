babf4c6a0bd12976edc77245444b5c52c1c47160
msalmanzafar <m.salman.zafar@gmail.com>
2016-11-29 01:38:05 +0500

    todo app update

--- Mass training lab task/TodoApp/app.js
+++ Mass training lab task/TodoApp/app.js
@@ -1,23 +1,34 @@
+var newArray=[];
+
 
 function addnewList(){
     var listItem = document.createElement("li");
     var inputValue = document.getElementById("todoInput").value;
     listItem.innerText = inputValue;
+    newArray.push(listItem);
     if (inputValue == "" || inputValue == " " ){
-        alert("Please Write Something...!");
+        alert("Please Write Something..!");
     }
     else{
         var text = document.getElementById("todoList");
         text.appendChild(listItem);
     }   
-    
 }
-
-
 var  newAction = document.getElementById("butn");
 newAction.onclick = addnewList;
 
-function focus(){
-    var a = "Writing";
-    document.getElementById("info").innerHTML=a;
+var remove = document.getElementById("remove");
+remove.onclick = removeAll;
+
+function removeAll(){
+    if (newArray.length == 0){
+        alert("Not have any List");
+    }
+    else
+        document.getElementById("todoList").innerHTML="";
+        newArray=[];
 }
+
+window.onload = function() {
+  document.getElementById("todoInput").focus();
+};
