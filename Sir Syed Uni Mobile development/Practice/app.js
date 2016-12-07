32e8826952b4372167064b42853a79aeeed6fb82
msalmanzafar <m.salman.zafar@gmail.com>
2016-12-07 21:54:44 +0500

    practice for javascript

---
+++ Sir Syed Uni Mobile development/Practice/app.js
@@ -0,0 +1,14 @@
+// var name1; // variable declared 
+// name ="Salman"; // assigning a value
+
+// alert(name);
+
+// var message = "Wlecome ";
+// var name = "Salman";
+// alert(message + name);// concatinate the two variable
+
+
+
+function func(){
+    alert("Function Working.!");
+}
