10191a5f68be1abd562325f07cd8bcd1a6abc0be
msalmanzafar <m.salman.zafar@gmail.com>
2016-12-08 01:22:32 +0500

    apdate practice

--- Sir Syed Uni Mobile development/Practice/app.js
+++ Sir Syed Uni Mobile development/Practice/app.js
@@ -9,6 +9,48 @@
 
 
 
+
+function imgEvent(){
+    alert('This is Void Link');
+}
+
+function imgEvent1(){
+    alert('You Clicked Me..!');
+}
+
+function inputEvent(){
+    alert('You Clicked Me..!');
+}
+
 function func(){
     alert("Function Working.!");
+}
+function colorChange(){
+    document.getElementById("color").style.color='green';
+    document.getElementById("color").style.textDecoration="underline";
+}
+ document.getElementById("color").style.textDecoration="none";
+ document.getElementById("color").style.fontSize="1.5em";
+function colorDefault(){
+    document.getElementById("color").style.color='blue';
+    document.getElementById("color").style.textDecoration="none";
+}
+
+function bckColor(){
+    document.getElementById("bckColor").style.backgroundColor='Blue';
+}
+function bckDefualt(){
+    document.getElementById("bckColor").style.backgroundColor='white';
+}
+
+ function checkAddress(fieldId) {
+    if (document.getElementById(fieldId).value === "") {
+        alert("Email address required.");
+    }
+ }
+ function emailBckColor(){
+    document.getElementById("email").style.backgroundColor='Blue';
+}
+function emailBckDefualt(){
+    document.getElementById("email").style.backgroundColor='white';
 }
