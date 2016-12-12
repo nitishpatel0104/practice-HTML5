2baa1189b6ea7b13cdae4e2ca4fe4e2cfabc45fd
msalmanzafar <m.salman.zafar@gmail.com>
2016-12-12 23:13:20 +0500

    lab task

--- Sir Syed Uni Mobile development/Practice/app.js
+++ Sir Syed Uni Mobile development/Practice/app.js
@@ -54,3 +54,26 @@ function bckDefualt(){
 function emailBckDefualt(){
     document.getElementById("email").style.backgroundColor='white';
 }
+
+
+function fillCity() {
+    var cityName;
+    var zipEntered = document.getElementById("zip").value;
+    switch (zipEntered) {
+        case "60608" :
+            cityName = "Chicago";
+            break;
+        case "68114" :
+            cityName = "Omaha";
+            break;
+        case "53212" :
+            cityName = "Milwaukee";
+    }
+    document.getElementById("city").value = cityName;
+ }
+function zipBckColor(){
+    document.getElementById("zip").style.backgroundColor='Blue';
+}
+function zipBckDefualt(){
+    document.getElementById("zip").style.backgroundColor='white';
+}
---
+++ Sir Syed Uni Mobile development/lab1 for javaScript/app.js
@@ -0,0 +1,65 @@
+// // variable declaration and assigning a string
+// var userName = "Salman ";
+// var myName = "Zafar";
+
+// // variable declaration and assigning a string and disply alert message
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// // display user biodata
+// alert(userName + myName);
+// var studentAge = "20 years old";
+// alert(studentAge);
+// var uni = "SSUET";
+// alert(uni);
+// var course="Mobile & Cloud Computing - Module A";
+// alert(course);
+
+// // display message
+// var o = "PIZZA";
+// var r1 = "PIZZ";
+// var d = "PIZ";
+// var e = "PI";
+// var r2 = "P";
+// alert(o + "\n" + r1 + "\n" + d + "\n" + e + "\n" +r2);
+
+// //disply user Future 
+// var dev = "You will be a Software Developer";
+// var loc = "in USA,";
+// var mer = "married to ABC width 2 kids";
+// alert(dev + loc + mer); 
+
+// //display user Email
+// var message1 = "My Eamil Address is ";
+// var email = "Salman.zafar292@gmail.com";
+// alert(message1 + email);
+
+// //disply current working steps
+// var message2 = "I am trying to learn from the book";
+// var book = "'A smarter way to learn JavaScript'";
+// alert(message2 + book);
+
+// // concatinate 4 question
+// alert("My Name is " + userName + myName + "\n" + "I am " + studentAge + "\n" + "I study in " + uni + "\n" + course + " Classes");
+
+// disply message in browser through JavaScript
+var browser = "Yes! I can write HTML content through JavaScript" ;
+document.write("<h1>"+browser+"</h2>");
+
+// Visited site
+var visi = "you have Visited this site 21 time";
+alert(visi);
+
+var year= 1993;
+var myBirth = "My birth yesr is ";
+var variable = "Data type of my declared variable is number";
+document.write("<h2>"+myBirth + year + "<br/>" + variable + "</h2>"); 
+
+// Online Shopping
+var website = "www.WowDress.pk";
+var visitor = "Bilal Kiyani ";
+var order = "Ordered ";
+var product = "3 T-Shirt(s) on ";
+
+document.write("<h2>" + visitor + order + product + website + "</h2>");
