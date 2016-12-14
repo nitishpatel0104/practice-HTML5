c8089ab04575a4bf2882e5321cffa6821d260f62
msalmanzafar <m.salman.zafar@gmail.com>
2016-12-14 16:24:42 +0500

    lab tasks

--- Sir Syed Uni Mobile development/lab1 for javaScript/app.js
+++ Sir Syed Uni Mobile development/lab1 for javaScript/app.js
@@ -1,51 +1,74 @@
 // // variable declaration and assigning a string
-// var userName = "Salman ";
-// var myName = "Zafar";
+var userName = "Salman ";
+var myName = "Zafar";
+var studentAge = "20 years old";
+var uni = "SSUET";
+var course="Mobile & Cloud Computing - Module A";
+// variable declaration and assigning a string and disply alert message
+function question3(){
+    var message;
+    message = "Hello World";
+    alert(message);
+}
 
-// // variable declaration and assigning a string and disply alert message
-// var message;
-// message = "Hello World";
-// alert(message);
+// display user biodata
+function question4(){
+    alert(userName + myName);
+    alert(studentAge);
+    alert(uni);
+    alert(course);
+}
 
-// // display user biodata
-// alert(userName + myName);
-// var studentAge = "20 years old";
-// alert(studentAge);
-// var uni = "SSUET";
-// alert(uni);
-// var course="Mobile & Cloud Computing - Module A";
-// alert(course);
 
-// // display message
-// var o = "PIZZA";
-// var r1 = "PIZZ";
-// var d = "PIZ";
-// var e = "PI";
-// var r2 = "P";
-// alert(o + "\n" + r1 + "\n" + d + "\n" + e + "\n" +r2);
+// display message
+function question5(){
+    var o = "PIZZA";
+    var r1 = "PIZZ";
+    var d = "PIZ";
+    var e = "PI";
+    var r2 = "P";
+    alert(o + "\n" + r1 + "\n" + d + "\n" + e + "\n" +r2);
+}
 
-// //disply user Future 
-// var dev = "You will be a Software Developer";
-// var loc = "in USA,";
-// var mer = "married to ABC width 2 kids";
-// alert(dev + loc + mer); 
 
-// //display user Email
-// var message1 = "My Eamil Address is ";
-// var email = "Salman.zafar292@gmail.com";
-// alert(message1 + email);
+// disply user Future 
+function question6(){
+    var dev = "You will be a Software Developer";
+    var loc = "in USA, ";
+    var mer = "Married to ABC width 2 kids";
+    alert(dev + loc + mer); 
+}
 
-// //disply current working steps
-// var message2 = "I am trying to learn from the book";
-// var book = "'A smarter way to learn JavaScript'";
-// alert(message2 + book);
 
-// // concatinate 4 question
-// alert("My Name is " + userName + myName + "\n" + "I am " + studentAge + "\n" + "I study in " + uni + "\n" + course + " Classes");
+// display user Email
+function question7(){
+    var message1 = "My Eamil Address is ";
+    var email = "Salman.zafar292@gmail.com";
+    alert(message1 + email); 
+}
 
-// // disply message in browser through JavaScript
-// var browser = "Yes! I can write HTML content through JavaScript" ;
-// document.write("<h1>"+browser+"</h2>");
+
+// disply current working steps
+function question8(){
+    var message2 = "I am trying to learn from the book ";
+    var book = "'A smarter way to learn JavaScript'";
+    alert(message2 + book);
+}
+
+
+ // concatinate 4 question
+ function question9(){
+    alert("My Name is " + userName + myName + "\n" + "I am " + studentAge + "\n" + "I study in " + uni + "\n" + course + " Classes");
+}
+
+
+// disply message in browser through JavaScript
+ function question10(){
+    var browser = "Yes! I can write HTML content through JavaScript" ;
+    //document.write("<h1>"+browser+"</h2>");
+    document.getElementById("display").innerHTML= "<h1>"+browser+"</h2>";
+}
+
 
 // // Visited site
 // var visi = "you have Visited this site 21 time";
